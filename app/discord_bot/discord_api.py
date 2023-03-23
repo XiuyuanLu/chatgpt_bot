1680a26a5e526d231da6571440915f273bfcd09d
XiuyuanLu <276908010@qq.com>
2023-03-23 17:02:52 +0800

    fix message author bug

--- app/discord_bot/discord_api.py
+++ app/discord_bot/discord_api.py
@@ -14,14 +14,14 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print(message.content)
-        if message.auther == self.user:
+        if message.author == self.user:
             return
         command, user_message = None, None
         for text in ['/ai', '/bot', '/chatgpt']:
             if message.content.startswith(text):
                 command = message.content.split(' ')[0]
                 user_message = message.content.replace(text, '')
-                print(command,user_message)
+                print(command, user_message)
 
         if command == 'ai' or command == '/bot' or command == '/chatgpt':
             bot_response = chatgpt_response(prompt=user_message)
